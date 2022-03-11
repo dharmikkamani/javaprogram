74cfc5bddc7383ef4eaa4a5c3e679fdc3aa971b0
Dharmik Kamani <95537988+dharmikkamani@users.noreply.github.com>
2022-03-11 19:29:11 +0530

    Update First.java

--- First.java
+++ First.java
@@ -1,6 +1,45 @@
 package Airth;
 
-public class First
+class Fourth
+{
+	public void divi(int a, int b)
+	{
+		int ans = a/b;
+		System.out.println("Division of 2 Numbers Is :" + ans);
+	}
+	
+}
+class Third extends Fourth
+{
+	public void multi(int a, int b)
+	{
+		int ans = a*b;
+		System.out.println("Multiplication of 2 Numbers Is :" + ans);
+	}
+	
+	public void multi(int a, int b, int c)
+	{
+		int ans = a*b*c;
+		System.out.println("Multiplication Of 3 Numbers is :" + ans);
+	}
+}
+
+class Second extends Third
+{
+	public void sub(int a, int b)
+	{
+		int ans = a-b;
+		System.out.println("Substraction of 2 Numbers Is :" + ans);
+	}
+	
+	public void sub(int a, int b, int c)
+	{
+		int ans = a-b-c;
+		System.out.println("Substraction Of 3 Numbers is :" + ans);
+	}
+}
+
+public class First extends Second
 {
 	public void add(int a, int b)
 	{
@@ -15,18 +54,4 @@ public class First
 	}
 }
 
-class Third
-{
-	public void multi(int a, int b)
-	{
-		int ans = a*b;
-		System.out.println("Multiplication of 2 Numbers Is :" + ans);
-	}
-	
-	public void multi(int a, int b, int c)
-	{
-		int ans = a*b*c;
-		System.out.println("Multiplication Of 3 Numbers is :" + ans);
-	}
-}
 
